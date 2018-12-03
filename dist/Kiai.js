23665354b64fe507cbee19c5b4cdadcac64c633a
Richard Heuser <richard.heuser@mediamonks.com>
2018-12-03 15:16:11 +0100

    added prepack script

--- dist/Kiai.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const App_1 = require("./lib/common/App");
-exports.default = App_1.default;
-//# sourceMappingURL=Kiai.js.map
